4a271c4d38baa4168ced31dd52e29e83a8c743b2
Christian Løverås <cl@superelectric.net>
2021-02-03 17:11:03 +0100

    WIP: Callbacks

--- code-example/signup-api-callback-example.py
+++ code-example/signup-api-callback-example.py
@@ -7,6 +7,13 @@ from flask import Flask, request
 # Create the Flask app
 app = Flask(__name__)
 
+# Handle incoming POST requests to the URL specified in the original POST to Vipps.
 @app.route('/vipps-signup-api/vipps-signup-callback', methods=['POST'])
-def json_example():
-    return 'JSON Object Example'
+
+def vipps_signup_callback():
+    content = request.get_json()
+    print(content)
+
+# Run
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
