145a24938d6ead58ff65ee5c58e6b5092fe66bbe
Christian Løverås <cl@superelectric.net>
2021-02-03 15:27:06 +0100

    A simple example

--- code-example/swagger_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.signup_api import SignupApi
-from swagger_client.api.signup_callback_api import SignupCallbackApi
