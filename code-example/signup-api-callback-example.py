904f7d0f21f4af919917dbea0ee61d526ffd80a6
Christian Løverås <cl@superelectric.net>
2021-02-03 17:15:29 +0100

    WIP: Flask details

--- code-example/signup-api-callback-example.py
+++ code-example/signup-api-callback-example.py
@@ -2,6 +2,7 @@
 # Handle incoming callbacks from Vipps when a merchant has signed.
 # https://github.com/vippsas/vipps-signup-api
 
+# Flask: https://flask.palletsprojects.com/en/1.1.x/
 from flask import Flask, request
 
 # Create the Flask app
@@ -9,10 +10,12 @@ app = Flask(__name__)
 
 # Handle incoming POST requests to the URL specified in the original POST to Vipps.
 @app.route('/vipps-signup-api/vipps-signup-callback', methods=['POST'])
-
 def vipps_signup_callback():
     content = request.get_json()
     print(content)
+    # TODO:
+    # Verify the signupCallbackToken: content['signupCallbackToken'].
+    # Do your processing.
 
 # Run
 if __name__ == '__main__':
